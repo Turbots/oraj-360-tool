bec80be3b1b8460a81eb0f7854b5497a1f2568d9
Andreas Evers <krosan@gmail.com>
2015-05-12 21:31:43 +0200

    Added embedded mongo for timeline service

--- backend/threesixtytool/timeline-service/src/main/java/be/ordina/threesixty/timeline/config/MongoConfiguration.java
+++ backend/threesixtytool/timeline-service/src/main/java/be/ordina/threesixty/timeline/config/MongoConfiguration.java
@@ -4,12 +4,15 @@ import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,7 +22,8 @@ import java.util.List;
  * Created by stevedezitter on 21/04/15.
  */
 @Configuration
-@ComponentScan("be.ordina.threesixty.person.repository")
+@Profile("default")
+@EnableMongoRepositories(basePackages={"be.ordina.threesixty.timeline.repository"})
 public class MongoConfiguration extends AbstractMongoConfiguration{
 
     @Bean
