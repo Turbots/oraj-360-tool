724c21ddbe8b98b76ac981eaa2c62b9391d4dbc2
andreasevers <andreas.evers@ordina.be>
2015-05-15 19:45:29 +0200

    Added mock stream for hystrix

--- backend/threesixtytool/hystrix/src/main/java/be/ordina/threesixty/hystrix/HystrixApplication.java
+++ backend/threesixtytool/hystrix/src/main/java/be/ordina/threesixty/hystrix/HystrixApplication.java
@@ -2,8 +2,10 @@ package be.ordina.threesixty.hystrix;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,13 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HystrixApplication {
 
-    @RequestMapping("/")
-    public String home() {
-        return "forward:/hystrix/index.html";
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HystrixApplication.class).web(true);
     }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(HystrixApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public ServletRegistrationBean mockStreamServlet() {
+        return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
     }
 
 }
