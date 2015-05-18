97a675d3c6a9b4eba8cc8d3b18c7af6c3b797dc3
andreasevers <andreas.evers@ordina.be>
2015-05-18 16:47:30 +0200

    Added admin microservice

---
+++ backend/threesixtytool/admin/src/main/java/be/ordina/threesixty/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package be.ordina.threesixty.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
